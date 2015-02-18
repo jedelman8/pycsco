4a76b407b2a4a3d86f149a922b533f0ef62fc74d
jedelman8 <jedelman8@gmail.com>
2015-02-17 16:48:28 -0800

    updated version

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import find_packages
 setup(
   name='pycsco',
   packages=find_packages(),
-  version='0.1.1',
+  version='0.1.2',
   description='Python modules to simplify working with Cisco NX-OS devices ',
   author='Jason Edelman',
   author_email='jedelman8@gmail.com',
   url='https://github.com/jedelman8/pycsco',
-  download_url='https://github.com/jedelman8/pycsco/tarball/0.1.1',
+  download_url='https://github.com/jedelman8/pycsco/tarball/0.1.2',
   install_requires=[
       'xmltodict==0.9.2',
   ],
