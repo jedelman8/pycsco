34bfc62c1be5d7a095d521979f08e01f7b683b44
jedelman8 <jedelman8@gmail.com>
2015-02-17 16:26:24 -0800

    setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name='pycsco',
   packages=['pycsco'],
-  version='0.1',
+  version='0.11',
   description='Python modules to simplify working with Cisco NX-OS devices ',
   author='Jason Edelman',
   author_email='jedelman8@gmail.com',
   url='https://github.com/jedelman8/pycsco',
-  download_url='https://github.com/jedelman8/pycsco/tarball/0.1',
+  download_url='https://github.com/jedelman8/pycsco/tarball/0.11',
   install_requires=[
       'xmltodict==0.9.2',
   ],
