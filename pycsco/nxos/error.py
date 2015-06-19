b56a208801d20959ac1b6f64c712001d66657c19
Michael Ben-Ami <mike.benami@gmail.com>
2015-06-19 10:00:08 -0400

    attempt to fix indentation

--- pycsco/nxos/error.py
+++ pycsco/nxos/error.py
@@ -1,7 +1,7 @@
 class CLIError(Exception):
-	def __init__(self, err, msg):
-		self.err = err
-		self.msg = msg
+    def __init__(self, err, msg):
+        self.err = err
+        self.msg = msg
 
-	def __str__(self):
-		return 'NX-OS CLI Configuration Error\n' + self.err + ' ' + self.msg
+    def __str__(self):
+        return 'NX-OS CLI Configuration Error\n' + self.err + ' ' + self.msg
