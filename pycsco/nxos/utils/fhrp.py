cd9934b3b725319b79528c232aefba31683b9820
jedelman8 <jedelman8@gmail.com>
2015-06-22 14:35:12 -0700

    added fhrp and updated mcast

---
+++ pycsco/nxos/utils/fhrp.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+
+# Copyright 2015 Jason Edelman <jedelman8@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This module contains a number of functions.  There are zero classes
+at this time.  It should be noted that these functions were built for the sole
+purpose of supporting Ansible modules (github.com/jedelman8/nxos-ansible).
+While that is the case, many of them can also be used independently of
+Ansible.  You can simply do a 'from pycsco.nxos.utils.nxapi_lib import *'
+to see which functions can be used independent of Ansible.  Overtime, the goal
+is to make this more object oriented, efficient, and optimal for support
+Ansible modules and in addition, general development.
+
+"""
+try:
+    import xmltodict
+    from pycsco.nxos.device import Device
+except ImportError as e:
+    print '*' * 30
+    print e
+    print '*' * 30
+
+__all__ = []
+
+
+def get_vrrp_existing(device, interface):
+    """Gets vrrp groups configured on each interface
+
+    Args:
+        device (Device): This is the device object of an NX-API enabled device
+            using the Device class within device.py
+
+    Returns:
+        dict: k/v pairs in the form of interface/[group list]
+
+    """
+    command = 'show vrrp detail interface ' + interface
+    xmlReturnData = device.show(command)
+    result = xmltodict.parse(xmlReturnData[1])
+    vrrp = []
+    try:
+        get_data = result['ins_api']['outputs']['output']['body'].get(
+            'TABLE_vrrp_group')
+        # ['ROW_vrrp_group']
+
+        # print type(get_data[0])
+        # print get_data[0]['ROW_vrrp_group']
+        try:
+            for entry in get_data:
+                get_dict = entry['ROW_vrrp_group']
+                group = get_dict.get('sh_group_id')
+                vip = get_dict.get('sh_vip_addr')
+                priority = get_dict.get('sh_priority')
+                preempt = get_dict.get('sh_group_preempt')
+                auth = get_dict.get('sh_auth_text')
+                interval = get_dict.get('sh_adv_interval')
+
+                if preempt == 'Disable':
+                    preempt = False
+                elif preempt == 'Enable':
+                    preempt = True
+
+                temp = dict(group=group, vip=vip, priority=priority,
+                            preempt=preempt, auth=auth, interval=interval)
+                vrrp.append(temp)
+
+        except TypeError:
+            get_dict = get_data['ROW_vrrp_group']
+
+            group = get_dict.get('sh_group_id')
+            vip = get_dict.get('sh_vip_addr')
+            priority = get_dict.get('sh_priority')
+            preempt = get_dict.get('sh_group_preempt')
+            auth = get_dict.get('sh_auth_text')
+            interval = get_dict.get('sh_adv_interval')
+            if preempt == 'Disable':
+                preempt = False
+            elif preempt == 'Enable':
+                preempt = True
+            temp = dict(group=group, vip=vip, priority=priority,
+                        preempt=preempt, auth=auth, interval=interval)
+            vrrp.append(temp)
+
+    except (KeyError, AttributeError):
+        vrrp = []
+
+    return vrrp
+
+
+def get_commands_config_vrrp(delta):
+    """Gets commands to config vrrp on an interface
+
+    Args:
+        delta (dict): vrrp params to config
+
+    Returns:
+        list: ordered list of commands to config hsrp
+
+    """
+
+    command = None
+    commands = []
+
+    CMDS = {
+        'priority': 'priority {0}',
+        'preempt': 'preempt',
+        'vip': 'address {0}',
+        'interval': 'advertisement-interval {0}',
+        'auth': 'authentication text {0}'
+    }
+
+    for k, v in delta.iteritems():
+        if v:
+            command = CMDS.get(k).format(v)
+        else:
+            command = 'no ' + CMDS.get(k).format(v)
+
+        if command:
+            commands.append(command)
+
+    return commands
+
+
+def get_existing_vrrp(device, interface, group):
+
+    existing_groups = get_vrrp_existing(device, interface)
+    existing = {}
+    if existing_groups:
+        for specific_group in existing_groups:
+            if specific_group.get('group') == group:
+                existing = specific_group
+
+    return existing
+
+
+def get_commands_remove_vrrp(group):
+    """Gets commands to remove an hsrp on an interface
+
+    Args:
+        group (str): hsrp group
+
+    Returns:
+        list: ordered list of commands to remove the hsrp group
+
+    Note:
+        Specific for Ansible module(s).  Not to be called otherwise.
+
+    """
+    commands = []
+    commands.append('no vrrp ' + group)
+    return commands
+
+
+if __name__ == "__main__":
+
+    device = Device(ip='n9396-2', username='cisco',
+                    password='!cisco123!', protocol='http')
+
+    interface = 'vlan100'
+    test = get_vrrp_existing(device, interface)
+
+    import json
+    print json.dumps(test, indent=4)
--- pycsco/nxos/utils/mcast.py
+++ pycsco/nxos/utils/mcast.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 # Copyright 2015 Jason Edelman <jedelman8@gmail.com>
+# Network to Code, LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,10 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from pycsco.nxos.device import Device
 from pycsco.nxos.utils import legacy
-
-import json
+from pycsco.nxos.error import CLIError
 
 try:
     import xmltodict
@@ -26,11 +25,19 @@ except ImportError as e:
     print e
     print '*' * 30
 
-__all__ = []
+__all__ = ['get_igmp_defaults', 'get_igmp_global', 'get_igmp_snooping',
+           'get_igmp_snooping_defaults', 'get_igmp_interface',
+           'get_pim_interface_defaults', 'get_pim_interface',
+           'get_igmp_interface_defaults']
 
 
 def get_igmp_defaults():
+    """Retrieves IGMP default configuration parameters
 
+    Returns:
+        Dictionary
+
+    """
     flush_routes = False
     enforce_rtr_alert = False
 
@@ -43,6 +50,14 @@ def get_igmp_defaults():
 
 
 def config_igmp(delta):
+    """Generates command list to configure IGMP mainly for Ansible modules
+
+    Args:
+        delta (dict): key/values of IGMP settings to configure
+
+    Returns:
+        List
+    """
 
     CMDS = {
         'flush_routes': 'ip igmp flush-routes',
@@ -69,7 +84,7 @@ def get_igmp_global(device):
             using the Device class from pycsco
 
     Returns:
-        List
+        Dictionary
     """
     command = 'show run igmp'
     data = device.show(command, text=True)
@@ -150,6 +165,11 @@ def get_igmp_snooping(device):
 
 
 def get_igmp_snooping_defaults():
+    """Retrieves igmp snooping default configurations
+
+    Returns:
+        Dictionary
+    """
 
     group_timeout = 'dummy'
     report_supp = True
@@ -163,13 +183,21 @@ def get_igmp_snooping_defaults():
                 report_supp=report_supp, v3_report_supp=v3_report_supp,
                 group_timeout=group_timeout)
 
-    default = dict((param, value) for (param, value) in args.iteritems() if value is not None)
+    default = dict((param, value) for (param, value) in args.iteritems()
+                   if value is not None)
 
     return default
 
 
 def config_igmp_snooping(delta, existing, default=False):
+    """Generates command list to configure IGMP  snooping for Ansible modules
 
+    Args:
+        delta (dict): key/values of IGMP snooping settings to configure
+
+    Returns:
+        List
+    """
     CMDS = {
         'snooping': 'ip igmp snooping',
         'group_timeout': 'ip igmp snooping group-timeout {}',
@@ -209,6 +237,7 @@ def get_igmp_interface(device, interface):
     Args:
         device (Device): This is the device object of an NX-API enabled device
             using the Device class within device.py
+        interface (str): full name of the interface
 
     Returns:
         Dictionary
@@ -285,6 +314,15 @@ def get_igmp_interface(device, interface):
 
 
 def config_igmp_interface(delta):
+    """Generates command list to configure IGMP interface settings
+       for Ansible modules
+
+    Args:
+        delta (dict): key/values of IGMP snooping settings to configure
+
+    Returns:
+        List
+    """
 
     CMDS = {
         'version': 'ip igmp version {0}',
@@ -329,7 +367,12 @@ def config_igmp_interface(delta):
 
 
 def get_igmp_interface_defaults():
+    """Generates command list that will be used to default interface
+       configs used for IGMP
 
+    Returns:
+        List
+    """
     version = '2'
     startup_query_interval = '31'
     startup_query_count = '2'
@@ -351,13 +394,29 @@ def get_igmp_interface_defaults():
                 group_timeout=group_timeout, report_llg=report_llg,
                 immediate_leave=immediate_leave)
 
-    default = dict((param, value) for (param, value) in args.iteritems() if value is not None)
+    default = dict((param, value) for (param, value) in args.iteritems()
+                   if value is not None)
 
     return default
 
 
 def config_remove_oif(oif_prefix, oif_source, oif_routemap, found_both,
                       found_prefix):
+    """Generates command list to remove a static-oif configuration
+       for an Ansible module
+
+    Args:
+        oif_prefix (str): IP multicast prefix
+        oif_source (str): IP source
+        oif_routemap (str): name of routemap to use for filtering
+        found_both: (bool): Flag from ansible mod stating if a prefix
+                            and source are changing
+        found_prefix: (bool): Flag from ansible mod stating if only
+                              a prefix is changing
+
+    Returns:
+        List
+    """
 
     commands = []
     command = None
@@ -375,11 +434,358 @@ def config_remove_oif(oif_prefix, oif_source, oif_routemap, found_both,
 
     return commands
 
-if __name__ == "__main__":
 
-    device = Device(ip='n9396-2', username='cisco', password= '!cisco123!', protocol='http')
+def get_pim_interface(device, interface):
+    """Gets pim config for a given interface
 
-    interface = 'Ethernet1/33'
-    existing = get_igmp_interface(device, interface)
+    Args:
+        device (Device): This is the device object of an NX-API enabled device
+            using the Device class within device.py
+        interface (str): Full interface name
 
-    print json.dumps(existing, indent=4)
+    Returns:
+        Dictionary
+
+    """
+    command = 'show ip pim interface ' + interface
+    try:
+        xmlReturnData = device.show(command)
+    except CLIError:
+        return {}
+
+    result = xmltodict.parse(xmlReturnData[1])
+    pim_interface = {}
+
+    try:
+        get_data = result['ins_api']['outputs']['output']['body'].get(
+            'TABLE_iod')['ROW_iod']
+
+        if isinstance(get_data.get('dr-priority'), unicode) or \
+                isinstance(get_data.get('dr-priority'), str):
+            pim_interface['dr_prio'] = get_data.get('dr-priority')
+        else:
+            pim_interface['dr_prio'] = get_data.get('dr-priority')[0]
+
+        pim_interface['hello_interval'] = get_data.get('hello-interval-sec')
+        border = get_data.get('is-border')
+
+        if border == 'true':
+            pim_interface['border'] = True
+        elif border == 'false':
+            pim_interface['border'] = False
+
+        isauth = get_data.get('isauth-config')
+        if isauth == 'true':
+            pim_interface['isauth'] = True
+        elif isauth == 'false':
+            pim_interface['isauth'] = False
+
+        pim_interface['neighbor_policy'] = get_data.get('nbr-policy-name')
+        if pim_interface['neighbor_policy'] == 'none configured':
+            pim_interface['neighbor_policy'] = None
+
+        jp_in_policy = get_data.get('jp-in-policy-name')
+        pim_interface['jp_policy_in'] = jp_in_policy
+        if jp_in_policy == 'none configured':
+            pim_interface['jp_policy_in'] = None
+
+        if isinstance(get_data.get('jp-out-policy-name'), unicode) or \
+                isinstance(get_data.get('jp-out-policy-name'), str):
+            pim_interface['jp_policy_out'] = get_data.get('jp-out-policy-name')
+        else:
+            pim_interface['jp_policy_out'] = get_data.get(
+                'jp-out-policy-name')[0]
+
+        if pim_interface['jp_policy_out'] == 'none configured':
+            pim_interface['jp_policy_out'] = None
+
+    except (KeyError, AttributeError):
+        return {}
+
+    command = 'show run interface ' + interface
+    xmlReturnData = device.show(command, text=True)
+    result = xmltodict.parse(xmlReturnData[1])
+
+    get_data = result['ins_api']['outputs']['output']['body']
+    all_lines = get_data.split('\n')
+    jp_configs = []
+    neigh = None
+    for each in all_lines:
+        if 'jp-policy' in each:
+            jp_configs.append(str(each.strip()))
+        elif 'neighbor-policy' in each:
+            neigh = str(each)
+
+    pim_interface['neighbor_type'] = None
+    neigh_type = None
+    if neigh:
+        if 'prefix-list' in neigh:
+            neigh_type = 'prefix'
+        else:
+            neigh_type = 'routemap'
+    pim_interface['neighbor_type'] = neigh_type
+
+    len_existing = len(jp_configs)
+    list_of_prefix_type = len([x for x in jp_configs if 'prefix-list' in x])
+    jp_type_in = None
+    jp_type_out = None
+    jp_bidir = False
+    if len_existing == 1:
+        # determine type
+        last_word = jp_configs[0].split(' ')[-1]
+        if last_word == 'in':
+            if list_of_prefix_type:
+                jp_type_in = 'prefix'
+            else:
+                jp_type_in = 'routemap'
+        elif last_word == 'out':
+            if list_of_prefix_type:
+                jp_type_out = 'prefix'
+            else:
+                jp_type_out = 'routemap'
+        else:
+            jp_bidir = True
+            if list_of_prefix_type:
+                jp_type_in = 'prefix'
+                jp_type_out = 'routemap'
+            else:
+                jp_type_in = 'routemap'
+                jp_type_out = 'routemap'
+    else:
+        for each in jp_configs:
+            last_word = each.split(' ')[-1]
+            if last_word == 'in':
+                if 'prefix-list' in each:
+                    jp_type_in = 'prefix'
+                else:
+                    jp_type_in = 'routemap'
+            elif last_word == 'out':
+                if 'prefix-list' in each:
+                    jp_type_out = 'prefix'
+                else:
+                    jp_type_out = 'routemap'
+
+    pim_interface['jp_type_in'] = jp_type_in
+    pim_interface['jp_type_out'] = jp_type_out
+    pim_interface['jp_bidir'] = jp_bidir
+
+    return pim_interface
+
+
+def config_pim_interface(delta, existing, jp_bidir, isauth):
+    """Generates command list to configure PIM on an interface within an
+       Ansible module
+
+    Args:
+        delta (dict): k/v pairs to configure PIM on an interface
+        existing (dict): k/v pairs of existing configuration on the interface
+        jp_bidir (bool): flag to see if join-prune policy already exists
+                         on the interface with a single command (not in/out)
+
+    Returns:
+        List
+
+    """
+
+    command = None
+    commands = []
+
+    CMDS = {
+        'sparse': 'ip pim sparse-mode',
+        'dr_prio': 'ip pim dr-priority {0}',
+        'hello_interval': 'ip pim hello-interval {0}',
+        'hello_auth_key': 'ip pim hello-authentication ah-md5 {0}',
+        'border': 'ip pim border',
+        'jp_policy_out': 'ip pim jp-policy prefix-list {0} out',
+        'jp_policy_in': 'ip pim jp-policy prefix-list {0} in',
+        'jp_type_in': '',
+        'jp_type_out': '',
+        'neighbor_policy': 'ip pim neighbor-policy prefix-list {0}',
+        'neighbor_type': ''
+    }
+
+    if jp_bidir:
+        if delta.get('jp_policy_in') or delta.get('jp_policy_out'):
+            if existing.get('jp_type_in') == 'prefix':
+                command = 'no ip pim jp-policy prefix-list {0}'.format(
+                    existing.get('jp_policy_in')
+                    )
+            else:
+                command = 'no ip pim jp-policy {0}'.format(
+                    existing.get('jp_policy_in')
+                    )
+            if command:
+                commands.append(command)
+
+    for k, v in delta.iteritems():
+        if k in ['dr_prio', 'hello_interval', 'hello_auth_key', 'border',
+                 'sparse']:
+            if v:
+                command = CMDS.get(k).format(v)
+            elif k == 'hello_auth_key':
+                if isauth:
+                    command = 'no ip pim hello-authentication ah-md5'
+            else:
+                command = 'no ' + CMDS.get(k).format(v)
+
+            if command:
+                commands.append(command)
+        elif k in ['neighbor_policy', 'jp_policy_in', 'jp_policy_out', 'neighbor_type']:
+            if k in ['neighbor_policy', 'neighbor_type']:
+                temp = delta.get('neighbor_policy') or existing.get(
+                    'neighbor_policy')
+                if delta.get('neighbor_type') == 'prefix':
+                    command = CMDS.get(k).format(temp)
+                elif delta.get('neighbor_type') == 'routemap':
+                    command = 'ip pim neighbor-policy {0}'.format(temp)
+                elif existing.get('neighbor_type') == 'prefix':
+                    command = CMDS.get(k).format(temp)
+                elif existing.get('neighbor_type') == 'routemap':
+                    command = 'ip pim neighbor-policy {0}'.format(temp)
+            elif k in ['jp_policy_in', 'jp_type_in']:
+                temp = delta.get('jp_policy_in') or existing.get(
+                    'jp_policy_in')
+                if delta.get('jp_type_in') == 'prefix':
+                    command = CMDS.get(k).format(temp)
+                elif delta.get('jp_type_in') == 'routemap':
+                    command = 'ip pim jp-policy {0} in'.format(temp)
+                elif existing.get('jp_type_in') == 'prefix':
+                    command = CMDS.get(k).format(temp)
+                elif existing.get('jp_type_in') == 'routemap':
+                    command = 'ip pim jp-policy {0} in'.format(temp)
+            elif k in ['jp_policy_out', 'jp_type_out']:
+                temp = delta.get('jp_policy_out') or existing.get(
+                    'jp_policy_out')
+                if delta.get('jp_type_out') == 'prefix':
+                    command = CMDS.get(k).format(temp)
+                elif delta.get('jp_type_out') == 'routemap':
+                    command = 'ip pim jp-policy {0} out'.format(temp)
+                elif existing.get('jp_type_out') == 'prefix':
+                    command = CMDS.get(k).format(temp)
+                elif existing.get('jp_type_out') == 'routemap':
+                    command = 'ip pim jp-policy {0} out'.format(temp)
+            if command:
+                commands.append(command)
+        command = None
+
+    return commands
+
+
+def get_pim_interface_defaults():
+    """Generates command list that will be used to default interface
+       configs (not the policies) used for PIM including prioroity,
+       border, hello interface, and hello auth.
+
+    Args:
+        existing (dict): key/values of the existing config
+        jp_bidir (bool): flag to detrmine if join-prune policy
+                         is currently configured with a single command
+                         and applied in both directions
+
+    Returns:
+        List
+    """
+    dr_prio = '1'
+    border = False
+    hello_interval = '30'
+    hello_auth_key = False
+
+    args = dict(dr_prio=dr_prio, border=border,
+                hello_interval=hello_interval,
+                hello_auth_key=hello_auth_key)
+
+    default = dict((param, value) for (param, value) in args.iteritems()
+                   if value is not None)
+
+    return default
+
+
+def default_pim_interface_policies(existing, jp_bidir):
+    """Generates command list that will be used default interface
+       policies used for PIM such as neighbor and join-prune policies
+
+    Args:
+        existing (dict): key/values of the existing config
+        jp_bidir (bool): flag to detrmine if join-prune policy
+                         is currently configured with a single command
+                         and applied in both directions
+
+    Returns:
+        List
+    """
+    commands = []
+
+    if jp_bidir:
+        if existing.get('jp_policy_in') or existing.get('jp_policy_out'):
+                if existing.get('jp_type_in') == 'prefix':
+                    command = 'no ip pim jp-policy prefix-list {0}'.format(
+                        existing.get('jp_policy_in')
+                        )
+        if command:
+            commands.append(command)
+
+    elif not jp_bidir:
+        command = None
+        for k, v in existing.iteritems():
+            if k == 'jp_policy_in':
+                if existing.get('jp_policy_in'):
+                    if existing.get('jp_type_in') == 'prefix':
+                        command = 'no ip pim jp-policy prefix-list {0} in'.format(
+                            existing.get('jp_policy_in')
+                            )
+                    else:
+                        command = 'no ip pim jp-policy {0} in'.format(
+                            existing.get('jp_policy_in')
+                            )
+            elif k == 'jp_policy_out':
+                if existing.get('jp_policy_out'):
+                    if existing.get('jp_type_out') == 'prefix':
+                        command = 'no ip pim jp-policy prefix-list {0} out'.format(
+                            existing.get('jp_policy_out')
+                            )
+                    else:
+                        command = 'no ip pim jp-policy {0} out'.format(
+                            existing.get('jp_policy_out')
+                            )
+            if command:
+                commands.append(command)
+            command = None
+
+    if existing.get('neighbor_policy'):
+        command = 'no ip pim neighbor-policy'
+        commands.append(command)
+
+    return commands
+
+
+def config_pim_interface_defaults(existing, jp_bidir, isauth):
+    """Generates command list to configure PIM interface settings
+       to be used with Ansible modules
+
+    Args:
+        existing (dict): key/values of the existing config
+        jp_bidir (bool): flag to detrmine if join-prune policy
+                         is currently configured with a single command
+                         and applied in both directions
+        isauth (bool):   flag that states if auth is currently in place
+                         for the hellos
+
+    Returns:
+        List
+    """
+    command = []
+
+    # returns a dict
+    defaults = get_pim_interface_defaults()
+    delta = dict(set(defaults.iteritems()).difference(
+                                                     existing.iteritems()))
+    if delta:
+        # returns a list
+        command = config_pim_interface(delta, existing,
+                                       jp_bidir, isauth)
+    comm = default_pim_interface_policies(existing, jp_bidir)
+    if comm:
+        for each in comm:
+            command.append(each)
+
+    return command
