432694c9bea91bbb644e17a316a524f76bb0b485
jedelman8 <jedelman8@gmail.com>
2015-11-01 05:48:49 -0800

    added pypi files

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
   name='pycsco',
   packages=find_packages(),
-  version='0.2.9',
+  version='0.3.0',
   description='Python modules to simplify working with Cisco NX-OS devices ',
   author='Jason Edelman',
   author_email='jedelman8@gmail.com',
   url='https://github.com/jedelman8/pycsco',
-  download_url='https://github.com/jedelman8/pycsco/tarball/0.2.9',
+  download_url='https://github.com/jedelman8/pycsco/tarball/0.3.0',
   package_data={'pycsco': ['nxos/utils/textfsm_templates/*.tmpl']},
   install_requires=[
       'xmltodict==0.9.2',
